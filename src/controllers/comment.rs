ef05094fbf504edf28fdbefbf4bf5241c035577f
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-12 21:00:17 +0900

    feat: クエリを使ったルーティングのテスト

--- src/controllers/comment.rs
+++ src/controllers/comment.rs
@@ -1,5 +1,14 @@
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
 
-pub async fn comment() -> impl Responder {
-    HttpResponse::Ok().body("Comment controller.")
+pub async fn getComments() -> impl Responder {
+    HttpResponse::Ok().body("get CommentList controller.")
+}
+
+pub async fn getComment(req:HttpRequest) -> impl Responder {
+    let id:u8 = req.match_info().get("id").unwrap().parse().unwrap();
+    HttpResponse::Ok().body(format!("get Comment controller. id={0}",id))
+}
+
+pub async fn postComment() -> impl Responder {
+    HttpResponse::Ok().body("Post Comment controller.")
 }
--- src/routes.rs
+++ src/routes.rs
@@ -3,5 +3,7 @@ use crate::controllers::{index, comment};
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::get().to(index::index));
-    cfg.route("/comment", web::get().to(comment::comment));
+    cfg.route("/comment", web::get().to(comment::getComments));
+    cfg.route("/comment/{id}", web::get().to(comment::getComment));
+    cfg.route("/comment",web::post().to(comment::postComment));
 }
