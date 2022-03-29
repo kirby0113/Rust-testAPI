fbca80bae746e700b0b7ce20af13b5c4f65268d0
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-29 23:29:02 +0900

    fix: モデル修正

--- src/models/category.rs
+++ src/models/category.rs
@@ -1,5 +1,8 @@
+use serde::{Serialize, Deserialize};
+use mongodb::bson::oid::ObjectId;
+
 #[derive(Debug, Serialize, Deserialize, Clone)]
-pub struct User {
+pub struct Category {
     pub _id: Option<ObjectId>,
-    pub name: String;
+    pub name: String,
 }
--- src/models/task.rs
+++ src/models/task.rs
@@ -1,11 +1,14 @@
-#[derive(Debug, Serialize, Deserialize, Clone)]
+use chrono::{Utc,Date};
+use mongodb::bson::oid::ObjectId;
+
+#[derive(Debug, Clone)]
 pub struct User {
     pub _id: Option<ObjectId>,
-    pub title: String;
-    pub category: String;
-    pub detail: String;
-    pub limit: String;
-    pub created_at: Date;
-    pub checked: Boolean;
-    pub isFavorite: Boolean;
+    pub title: String,
+    pub category: String,
+    pub detail: String,
+    pub limit: String,
+    pub created_at: Date<Utc>,
+    pub checked: bool,
+    pub isFavorite: bool,
 }
