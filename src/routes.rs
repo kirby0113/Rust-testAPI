58a53b8f015ae8904c65abce5842332c946dd84d
kirby0113 <2000ryo0113@ezweb.ne.jp>
2022-03-29 23:30:21 +0900

    feat: ルーティング追加

--- src/routes.rs
+++ src/routes.rs
@@ -1,9 +1,10 @@
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
-use crate::controllers::{index, comment};
+use crate::controllers::{index, comment,category};
 
 pub fn routes(cfg: &mut web::ServiceConfig) {
     cfg.route("/", web::get().to(index::index));
     cfg.route("/comment", web::get().to(comment::getComments));
     cfg.route("/comment/{id}", web::get().to(comment::getComment));
     cfg.route("/comment",web::post().to(comment::postComment));
+    cfg.route("/categories",web::get().to(category::getCategories));
 }
